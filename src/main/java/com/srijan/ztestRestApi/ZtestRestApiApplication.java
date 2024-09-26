960f17c9fdc2f20d170a07c7cabf6ce614c81cf7
Sr1jan27 <singhsrijan809@gmail.com>
2024-09-26 09:32:30 +0530

    first commit

---
+++ src/main/java/com/srijan/ztestRestApi/ZtestRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.srijan.ztestRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtestRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtestRestApiApplication.class, args);
+	}
+
+}
